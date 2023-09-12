e85a2e1d63cee3e2bc55355e11bd89197a0bd9b3
Mark Michaelis <mallenm1993@gmail.com>
2023-09-11 21:03:12 -0400

    Add User model and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create([
+  { email: "mark@mark.mark",
+    password: "baseball"
+  },
+  { email: "peter@peter.peter",
+    password: "cornhole"
+  },
+  { email: "steve@steve.steve",
+    password: "tennis"
+  },
+  { email: "char@char.char",
+    password: "basketball"
+  },
+  { email: "wes@wes.wes",
+    password: "crossfit"
+  },
+  { email: "mac@mac.mac",
+    password: "soccer"
+  },
+  { email: "jared@jared.jared",
+    password: "poetry"
+  },
+  { email: "marshall@marshall.marshall",
+    password: "driving"
+  },
+  { email: "jeff@jeff.jeff",
+    password: "cornhole"
+  },
+  { email: "roper@roper.roper",
+    password: "beats"
+  },
+  { email: "burko@burko.burko",
+    password: "golf"
+  }
+])
