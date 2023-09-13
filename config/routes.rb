70492518f3eaaa03434b9618e1fe8dce3c930cfb
Mark Michaelis <mallenm1993@gmail.com>
2023-09-13 16:06:05 -0400

    Adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users do
-    resources :profiles
-  end
+  devise_for :users 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "posts#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users
+  resources :profiles
   resources :posts
   resources :comments
   resources :likes
