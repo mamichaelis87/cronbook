de3176b3d92701b4e327ead5679239f5ba7235c9
Mark Michaelis <mallenm1993@gmail.com>
2023-09-13 16:00:07 -0400

    Generate controllers and add initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    resources :profiles
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "posts#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :posts
+  resources :comments
+  resources :likes
+  resources :friendships
 end
