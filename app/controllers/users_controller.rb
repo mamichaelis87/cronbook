73b9bcabf1d6c332c42fa4fa191e84ac71ad58ae
Mark Michaelis <mallenm1993@gmail.com>
2023-09-18 12:47:24 -0400

    Add index page for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,16 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @profile = @user.profile
+    @post = Post.new
+  end
+
+  def index
+    @users = User.all
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(profile_attributes: [:first_name, :last_name, :birthday, :hometown, :occupation, :quote, :other_info])
   end
 end
