9c9c7d3e513511e487071f8e9435fba6091667ec
Mark Michaelis <mallenm1993@gmail.com>
2023-09-20 16:17:37 -0400

    Add profile edit page and controller method

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,24 @@
 class ProfilesController < ApplicationController
+  def new
+  end
+  
+  def edit
+    @profile = current_user.profile
+  end
+
+  def update
+    @profile = current_user.profile
+
+    if @profile.update(profile_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :birthday, :hometown, :occupation, :quote, :other_info)
+  end
 end
