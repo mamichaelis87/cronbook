a02e4d278fc54b3eb05a491d9bbf1d968fde49fc
Mark Michaelis <mallenm1993@gmail.com>
2023-09-20 14:50:50 -0400

    Set up posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,27 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:author_id, :body)
+  end
 end
