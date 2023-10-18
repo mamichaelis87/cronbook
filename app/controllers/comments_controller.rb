12b3ee299c6b17e8dda8d2ee3565fc2cda6025f9
Mark Michaelis <mallenm1993@gmail.com>
2023-10-18 13:42:21 -0400

    Add create method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      redirect_to post_path(@comment.post)
+    else
+      redirect_back(fallback_location: root_path)
+    end
+    
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:post_id, :body, :author_id)
+  end
+
 end
