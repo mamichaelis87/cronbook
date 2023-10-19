0c96654c147555d461e8d76834008e4a57bb884f
Mark Michaelis <mallenm1993@gmail.com>
2023-10-18 20:32:23 -0400

    Add remove friend method

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,2 +1,11 @@
 class FriendsController < ApplicationController
+  def index
+    @friends = current_user.friends
+  end
+
+  def destroy 
+    @friend = current_user.friends.find(params[:id])
+    current_user.remove_friend(@friend)
+    head :no_content
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,7 @@ class User < ApplicationRecord
   has_many :friend_requests, dependent: :destroy
   has_many :pending_friends, through: :friend_requests, source: :friend
 
-  
+  def remove_friend(friend)
+    current_user.friends.destroy(friend)
+  end
 end
