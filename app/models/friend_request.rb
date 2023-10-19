6ec0dd2711bbc984c2be156f4e8985c284e7b19c
Mark Michaelis <mallenm1993@gmail.com>
2023-10-18 20:41:38 -0400

    Add more validations

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -4,7 +4,9 @@ class FriendRequest < ApplicationRecord
   validates :user, presence: true
   validates :friend, presence: true, uniqueness: { scope: :user }
   validate :not_self
-  
+  validate :not_friends
+  validate :not_pending
+
   def accept
     user.friends << friend
     destroy
@@ -15,4 +17,13 @@ class FriendRequest < ApplicationRecord
   def not_self
     errors.add(:friend, "Cannot be equal to user") if user == friend
   end
+
+  def not_friends
+    errors.add(:friend, "Is already added") if user.friends.include?(friend)
+  end
+
+  def not_pending
+    errors.add(:friend, "Already requested Friendship") if friend.pending_friends.include?(user)
+  end
+
 end
