bfb77a13aca635b751c002408f4fbebfaa2e8e0f
Mark Michaelis <mallenm1993@gmail.com>
2023-10-19 11:08:34 -0400

    Move logic from view to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,30 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id]) # this is not always the current_user
-    if @user.profile
-      @profile = @user.profile
-    else
-      @profile = Profile.create(user_id: @user.id)
-    end
+    set_profile(@user)
+    @pending_friends = @user.pending_friends
+    @friend_requests = FriendRequest.where(friend: @user)
     @comment = Comment.new
   end
 
   def index
-    @users = User.all
+    @friends = current_user.friends
+    @pending_friends = current_user.pending_friends
+    @friend_requests = FriendRequest.where(friend: current_user)
+    @potential_friends = User.all
   end
 
   private
 
   def user_params
     params.require(:user).permit(profile_attributes: [:first_name, :last_name, :birthday, :hometown, :occupation, :quote, :other_info])
+  end
+
+  def set_profile(user)
+    if user.profile
+      @profile = user.profile
+    else
+      @profile = Profile.create(user_id: user.id)
+    end
   end
 end
