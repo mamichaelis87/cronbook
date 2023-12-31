6f21ffec03580afe4b65248ae35b2a1585b32ebc
Mark Michaelis <mallenm1993@gmail.com>
2023-12-30 21:45:32 -0500

    Limit comments to 3 unless on post/show

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,16 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User"
   has_many :comments
   has_many :likes, as: :likable
+
   def liked?(user)
     self.likes.find{|like| like.liker_id == user.id}
+  end
+
+  def recent_comments
+    if self.comments.length > 3 
+      return self.comments.last(3)
+    else
+      return self.comments
+    end
   end
 end
