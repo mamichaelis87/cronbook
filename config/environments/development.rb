61cdabec9a694c1c4a6cb43573d941f1bfc809be
Mark Michaelis <mallenm1993@gmail.com>
2024-01-20 19:04:36 -0500

    Configure action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
   # Configuration for devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,9 +66,21 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'https://cronbook-289dcf3d73cf.herokuapp.com/' }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:         'smtp.gmail.com',
+    port:            587,
+    domain:          'https://cronbook-289dcf3d73cf.herokuapp.com/',
+    user_name:       'mallenm1993@gmail.com',
+    password:        ENV['GMAIL_PASSWORD'],
+    authentication:  'plain',
+    enable_starttls: true,
+    open_timeout:    5,
+    read_timeout:    5 }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
